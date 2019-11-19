9bb397a81657fcc97a2ff7056ecc017210aef4bc
Karthikeyan <karthikeyan.live@hotmail.com>
2019-11-19 15:46:04 +0530

    reverted

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,27 +1,7 @@
-import React from 'react'
-import Layout from '../layouts/Main';
-import fetch from 'isomorphic-unfetch'
+import Nav from '../components/nav'
 
-import Post from '../components/Post'
-
-export function getPosts() {
-  return fetch('https://jsonplaceholder.typicode.com/posts')
-}
-
-const IndexPage = ({ posts }) => (
-  <Layout>
-    <ul>
-      {posts.map(p => (
-        <Post key={p.title} post={p} />
-      ))}
-    </ul>
-  </Layout>
-)
-
-IndexPage.getInitialProps = async ({ req }) => {
-  const res = await getPosts()
-  const json = await res.json()
-  return { posts: json }
-}
-
-export default IndexPage
+export default () =>
+  <>
+    <p>This page is served by the index function. This is a test commit message.</p>
+    <Nav />
+  </>
